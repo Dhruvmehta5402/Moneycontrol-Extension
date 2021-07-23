a6b61a840cf104a4fcc7eb0116b3374c354ef74e
dhruvm4 <dhruvm4@illinois.edu>
2021-07-23 19:39:17 +0530

    Works on Chrome successfully.

---
+++ content.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         MoneyControl Reccomendations
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description   Email Stock Recommendations
+// @author       You
+// @match        https://www.moneycontrol.com/
+// @icon         https://www.google.com/s2/favicons?domain=moneycontrol.com
+// @grant        none
+// ==/UserScript==
+
+
+
+var table = document.getElementsByClassName('stkAdv_tbl');
+var links = table[0].getElementsByTagName("A");
+var message = "Stocks: \n";
+var articles = "Articles: \n";
+for (let i = 0; i < links.length; i++) {
+    if (numWords(String(links[i].text)) < 4) {
+        console.log(links[i]);
+        message = message + String(links[i].text) + "\n";
+    } else {
+        articles = articles + links[i] + "\n";
+    }
+}
+console.log(message);
+message = message + "\n";
+message = message + articles;
+
+function sendMail() {
+    var link = "mailto:dhruvmehtakolkata@gmail.com"
+             + "?cc=botweb91@gmail.com"
+             + "&subject=" + encodeURIComponent("Moneycontrol Stock Reccomendations")
+             + "&body=" + encodeURIComponent(message)
+    ;
+    window.location.href = link;
+}
+
+function numWords(sentence) {
+    let count = 0;
+    for (let i = 0; i < sentence.length; i++) {
+        if (sentence.charAt(i) === ' ') {
+            count ++;
+        }
+    }
+    return count + 1;
+}
+sendMail();
